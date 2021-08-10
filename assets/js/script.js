b24b076dd866b454ccdee83794780cd447b0d634
Theo <trgrf1@gmail.com>
2021-08-10 15:23:31 -0400

    saving skeleton additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,44 @@
 // Need variables to keep track of quiz
 
+
 // needs variables to re. the Dom Elements
+var startButton = document.getElementById('start-button');
+var quizEl = document.getElementById('quiz-time');
+var endQuizEl = document.getElementById('game-over');
+
 
 // Need function to start quiz
+function quizstart() {
 
+}
 // create a function get questions
+function questions() {
 
+}
 // function to check if user was right or wrong
     // check how many questions there are if there are no more questions end quiz, if more questions get next question
 
+function answers() {
+
+}
+
 // need a function to end the quiz
+function quizend() {
+
+}
 
 // function that ends quiz when time has hit 0
+function timeover() {
+
+}
+
 
 // function to save the high score to local storage
+function highscores() {
+
+}
+
+
 var questions = [
 {
     title: "", 
@@ -48,5 +73,5 @@ var questions = [
 ]
 
 for (var i = 0; i < questions.length; i++) {
-    
+
 }
