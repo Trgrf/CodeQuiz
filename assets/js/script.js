f2c11a693858f5632e2cd255d962cc8d3243880b
Theo <trgrf1@gmail.com>
2021-08-10 14:48:20 -0400

    saving additions

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// Need variables to keep track of quiz
+
+// needs variables to re. the Dom Elements
+
+// Need function to start quiz
+
+// create a function get questions
+
+// function to check if user was right or wrong
+    // check how many questions there are if there are no more questions end quiz, if more questions get next question
+
+// need a function to end the quiz
+
+// function that ends quiz when time has hit 0
+
+// function to save the high score to local storage
+var questions = [
+{
+    title: "", 
+    choices: [],
+    answer: "",
+
+},
+{
+    title: "", 
+    choices: [],
+    answer: "",
+
+},
+{
+    title: "", 
+    choices: [],
+    answer: "",
+
+},
+{
+    title: "", 
+    choices: [],
+    answer: "",
+
+},
+{
+    title: "", 
+    choices: [],
+    answer: "",
+
+},
+]
