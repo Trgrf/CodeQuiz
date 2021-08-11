6edc9df580a5bdbc19837cd49b54ff2514de340e
Theo <trgrf1@gmail.com>
2021-08-11 16:07:33 -0400

    saving functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,74 @@
 // Need variables to keep track of quiz
-
+var time = 0;
+var timerCount;
+var score = 0;
+var index = 0;
 
 // needs variables to re. the Dom Elements
-var startButton = document.getElementById('start-button');
-var quizEl = document.getElementById('quiz-time');
+var startButtonEl = document.getElementById('start-button');
+var quizTimeEl = document.createElement('h2');
 var endQuizEl = document.getElementById('game-over');
+var quizScreenEl = document.getElementById('quizscreen');
 
 
 // Need function to start quiz
 function quizstart() {
+    timerCount = 10;
+    var startScreenEl = document.getElementById('startscreen')
+    startScreenEl.classList.add("hide");
 
+    quizScreenEl.classList.remove('hide');
+    starttimer();
+}
+
+function starttimer() {
+    var timeSpanEl = document.createElement("span")
+    var largeFont = document.querySelector(".large-font")
+    largeFont.appendChild(timeSpanEl.appendChild(quizTimeEl));
+    var timerInterval = setInterval(function () {
+        timerCount--;
+        quizTimeEl.textContent = timerCount;
+        if (timerCount <= 0) {
+            timerCount = 0;
+            quizTimeEl.textContent = timerCount;
+        }
+
+        // if (timerCount === 0) {
+        //     clearInterval(timer);
+        //     quizend();
+
+    }, 1000)
+    
+    questions();
 }
 // create a function get questions
 function questions() {
+    quizScreenEl.innerHTML = ""
+    var questionTextEl = document.createElement('h3');
+    questionTextEl.textContent = questionsArr[index].title;
+    quizScreenEl.appendChild(questionTextEl);
+    questionsArr[index].choices.forEach(function (choice) {
+        var choiceBtn = document.createElement("button")
+        choiceBtn.textContent = choice;
+        choiceBtn.onclick = answers;
+        choiceBtn.setAttribute("value", choice)
 
+        questionTextEl.appendChild(choiceBtn);
+
+    })
 }
 // function to check if user was right or wrong
-    // check how many questions there are if there are no more questions end quiz, if more questions get next question
+// check how many questions there are if there are no more questions end quiz, if more questions get next question
 
 function answers() {
+    console.log(this.value);
+    if (this.value != questionsArr[index].answer) {
+        console.log("Wrong Answer!")
+    }
+
+
+    index++;
+    questions();
 
 }
 
@@ -39,39 +89,45 @@ function highscores() {
 }
 
 
-var questions = [
-{
-    title: "Who won the Rainbow 6 Siege Inviational 2020?", 
-    choices: ['SSG', 'G2', 'TSM', 'NIP'],
-    answer: "SSG",
+var questionsArr = [
+    {
+        title: "Who won the Rainbow 6 Siege Inviational 2020?",
+        choices: ['SSG', 'G2', 'TSM', 'NIP'],
+        answer: "SSG",
 
-},
-{
-    title: "What is the longest running anime?", 
-    choices: ['Naruto', 'MHA', 'OnePiece', 'Bleach'],
-    answer: "OnePiece",
+    },
+    {
+        title: "What is the longest running anime?",
+        choices: ['Naruto', 'MHA', 'OnePiece', 'Bleach'],
+        answer: "OnePiece",
 
-},
-{
-    title: "Where is the Statue of Liberty located?", 
-    choices: ['Paris', 'Italy', 'Chicago', 'NewYork'],
-    answer: "NewYork",
+    },
+    {
+        title: "Where is the Statue of Liberty located?",
+        choices: ['Paris', 'Italy', 'Chicago', 'NewYork'],
+        answer: "NewYork",
 
-},
-{
-    title: "What color is the sky?", 
-    choices: ['Black', 'Pink', 'Blue', 'Aqua'],
-    answer: "Blue",
+    },
+    {
+        title: "What color is the sky?",
+        choices: ['Black', 'Pink', 'Blue', 'Aqua'],
+        answer: "Blue",
 
-},
-{
-    title: "How fun is coding?", 
-    choices: ['Insanly', 'Moderately', 'Super', 'Incredibly'],
-    answer: "Insanely",
+    },
+    {
+        title: "How fun is coding?",
+        choices: ['Insanly', 'Moderately', 'Super', 'Incredibly'],
+        answer: "Insanely",
 
-},
+    },
 ]
 
-for (var i = 0; i < questions.length; i++) {
 
-}
+
+
+
+
+
+
+
+startButtonEl.addEventListener("click", quizstart)
